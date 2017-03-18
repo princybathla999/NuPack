3340b091d76b8a25ea30158d2a8063242cb78fd3
Princy Bathla <princybathla999@yahoo.com>
2017-03-18 15:31:50 -0400

    specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "bundler/setup"
 require "Nupack"
-
-RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-end
